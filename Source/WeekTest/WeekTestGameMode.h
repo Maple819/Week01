3e64726d91f68bec8be7831bc18e002030201994
Maple <2931527112@qq.com>
2024-11-22 22:41:40 +0800

    Week01

---
+++ Source/WeekTest/WeekTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WeekTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWeekTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWeekTestGameMode();
+};
+
+
+
